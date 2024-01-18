51b8e5b3aee1e9f26482d5a6dd3acb7f60fb01da
Cerberos4238 <105981834+Cerberos4238@users.noreply.github.com>
2024-01-18 18:58:37 +0100

    Add files via upload

---
+++ Fast_Boot_All.ps1
@@ -0,0 +1,27 @@
+﻿#Déclaration des variables et du chemin de la clé de registre
+$value= ""
+$Key = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+
+$value = Get-ItemPropertyValue -Path $key -Name FastBoot -ErrorAction SilentlyContinue
+
+#Vérification si la propriété Fastboot est présente ou non
+if ($value -eq "0") {
+    Write-host `r`n"Le demarrage rapide est inactif"
+    Exit 0
+}
+Else{
+    Write-host `r`n"Le demarrage rapide est actif"
+
+        #Variable chemin du registre 
+$HKLM_path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+
+#Se placer dans le registre souhaité
+Set-Location -Path $HKLM_path
+
+
+#Création de la propriété Fastboot pour la désactivée.
+New-ItemProperty -Path $HKLM_path -Name 'FastBoot' -Value "0" -PropertyType DWORD -Force
+
+Write-host `r`n"Le demarrage rapide est maintenant inactif"
+    
+}
